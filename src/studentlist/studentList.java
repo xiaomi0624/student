bce6b29f12e6d14f18a652163e0dab6588d727b0
xiaomi0624 <98172969+xiaomi0624@users.noreply.github.com>
2022-02-05 02:09:56 -0500

    Update studentList.java

--- src/studentlist/studentList.java
+++ src/studentlist/studentList.java
@@ -4,11 +4,12 @@ package studentlist;
 
 public class studentList {
     public static void main(String[] args){
-        Student[] studentList = new Student[4];
+        Student[] studentList = new Student[5];
         studentList[0] = new Student("1","Ronak Sheth");
         studentList[1] = new Student("2","John Doe");
         studentList[2] = new Student("3","abc");
         studentList[3] = new Student("4","Michelle Zhang");
+        studentList[4] = new Student("5","Kathy Wang");
         
         for(int  i = 0;i<studentList.length;i++){
             System.out.println(studentList[i].getStudentName());

f4c0d5805532e88caf375ec24b20f91acafe4c30
hongmei Zhang <zhahongm@sheridanc.on.ca>
2022-02-05 01:51:21 -0500

    Revert "first"
    This reverts commit 2123e93a0dd783fb3a36ee0e9de6ae2fe7760f58.

--- src/studentlist/studentList.java
+++ src/studentlist/studentList.java
@@ -4,12 +4,11 @@ package studentlist;
 
 public class studentList {
     public static void main(String[] args){
-        Student[] studentList = new Student[5];
+        Student[] studentList = new Student[4];
         studentList[0] = new Student("1","Ronak Sheth");
         studentList[1] = new Student("2","John Doe");
         studentList[2] = new Student("3","abc");
         studentList[3] = new Student("4","Michelle Zhang");
-        studentList[4] = new Student("5","kathy Wang");
         
         for(int  i = 0;i<studentList.length;i++){
             System.out.println(studentList[i].getStudentName());
